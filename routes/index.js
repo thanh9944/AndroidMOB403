1b742c565db5d52d75f158d4cdd1a08372e76027
Le Nhat Thanh <thanhdubai9944@gmail.com>
2022-07-23 17:10:16 +0700

    add1

--- routes/index.js
+++ routes/index.js
@@ -1,20 +1,27 @@
 var express = require('express');
 var router = express.Router();
-var fs=require('fs');
+var fs = require('fs');
 
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.get('/', function (req, res, next) {
+    res.render('index', {title: 'Express'});
 });
 
-router.get('/getUser',function (rep,res) {
-  fs.readFile('data/data.txt',function (err,data) {
-    if (err!=null){
-      res.send(err.message);
-    }else {
-      res.send(data.toString());
-    }
-  })
+router.get('/getUser', function (rep, res) {
+    fs.readFile('data/data.txt', function (err, data) {
+        if (err != null) {
+            res.send(err.message);
+        } else {
+            res.send(data.toString());
+        }
+    })
 })
+
+router.post('/createUser',function (req,res) {
+  var email=req.body.email;
+  var password=req.body.password;
+  res.send(email)
+})
+
 module.exports = router;
